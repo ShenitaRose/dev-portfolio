9c119dd9e106e3f179300983282ebebd130866df
ShenitaRose <simsationelle@gmail.com>
2022-11-01 23:28:33 +0000

    Added memoji

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,20 @@ export default function Home() {
 
             <main>
                 <div className="container mx-auto">
-                    <h1 className="text-3xl font-bold text-center mt-10">
-                        Hi, I&apos;m <span className="text-pink-600">Shenita</span>.
+
+                    <div className="flex items-center justify-center mt-20">
+                        <video className="h-60 w-60" autoPlay loop muted>
+                            <source src="memoji.mov" type="video/mp4"/>
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+
+                    <h1 className="text-3xl font-bold text-center mt-4">
+                        Hi, my name is <span className="text-pink-600">Shenita</span>.
                     </h1>
 
                     <p className="text-center mt-3">
-                        I&apos;m a front-end developer based in London.
+                        I&apos;m a front-end developer based in London. Currently focusing on <span className="text-pink-500 font-bold">nextJS</span>, and <span className="text-blue-500 font-bold">tailwindCSS</span>.
                     </p>
                 </div>
             </main>
